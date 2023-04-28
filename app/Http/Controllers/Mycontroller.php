bba739de7c68b9030feb62c7172e7a76a74b099c
Carmine Armenante <carmine.armenante@gmail.com>
2023-04-28 12:11:12 +0200

    flag

--- app/Http/Controllers/Mycontroller.php
+++ app/Http/Controllers/Mycontroller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class Mycontroller extends Controller
 {
@@ -13,5 +14,10 @@ class Mycontroller extends Controller
         return view('welcome',compact('title','articles'));
     }
 
+    public function selectLanguage($lang)
+    {
+        Session::put('language',$lang);
+        return redirect()->back();
+    }
 
 }
